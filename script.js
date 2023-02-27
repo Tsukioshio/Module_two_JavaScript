a57ac71b9d994c032d33a5e040ff624c8a2e7563
unknown <tsukioshio1@gmail.com>
2023-02-27 18:26:12 +0800

    'Дз-4'

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
-const firstNumber = Number.parseFloat(prompt('Введите первое число'));
-const twoNumber = Number.parseFloat(prompt('Введите второе число'));
+const week = [
+'Monday',
+'Tuesday',
+'Wednesday',
+'Thursday',
+'Friday',
+'Saturday',
+'Sunday',
+];
 
-alert((firstNumber + twoNumber).toFixed(1));
-alert((firstNumber - twoNumber).toFixed(1));
-alert((firstNumber / twoNumber).toFixed(1));
-alert((firstNumber * twoNumber).toFixed(1));
-alert((firstNumber % twoNumber).toFixed(1));
+const weekendtDayIndex = [5, 6];
+const currentDayIndex = new Date().getDay();
+
+
+function printDay(index) {
+    const classes = `text ${currentDayIndex === index ? 'italic' : ''} ${weekendtDayIndex.includes(index) ? 'bold' : ''}`
+document.writeln(`<div class='${classes}'>${week[index]}</div>`)
+}
+
+// week.forEach(printDay);
+
+for (let i = 0; i < week.length; i++) {
+    printDay(i);
+}
+
+
+
+
+const arr = ['1234','34536434','767456485','54364234','33246346','4564345','7787654645'];
+
+function checkFirstNumber(number) {
+    let firstNumber = number;
+
+    while(firstNumber > 10) firstNumber /= 10;
+
+    firstNumber = Math.floor(firstNumber);
+
+    return firstNumber === 3 || firstNumber === 7;
+}
+
+function print(item) {
+    console.log(item);
+}
+
+arr
+.filter((num)=>checkFirstNumber(num))
+.forEach(print);
