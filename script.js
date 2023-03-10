6b918f4930cc144576d19ce9ee2472f16147cf84
unknown <tsukioshio1@gmail.com>
2023-03-10 17:44:00 +0800

    'homework-07'

--- script.js
+++ script.js
@@ -1,46 +1,27 @@
-function print(text) {
-    document.write(`<div>${text}</div>`);
-    }
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
 
-const getAccumulatedIncome = (salary, extraMoney, expensesAmount) => (salary + extraMoney) - expensesAmount;
+let sum = 0;
 
-const getTargetMonth = (accumulatedIncome, purpose) => Math.ceil(purpose / accumulatedIncome);
+for (const key in salaries) {
+    sum += salaries[key]
+}
 
-const GetbudgetDay = (accumulatedIncome) => Math.floor(accumulatedIncome / 30);
+console.log(sum);
 
-const init = () => {
-    const salary = Number.parseFloat(prompt('Ваш месячный доход?'));
-    print(`Месячный доход: ${salary} Руб`);
-    
-    const extraMoney = Number.parseFloat(prompt(`Перечислите возможный доход за ваши дополнительные работы:`))
-    print(`Дополнительный доход: ${extraMoney} Руб`);
-    
-    const expenseCategories = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-    print(`Категории Расходов: ${expenseCategories}`);
-    
-    const expensesAmount = Number.parseFloat(prompt('Во сколько обойдутся обязательные статьи расходов?'));
-    print(`Сумма расходов: ${expensesAmount} Руб`);
+let studentScores = {
+    Коля: '99',
+    Вася: '35',
+    Петя: '70',
+    Таня: '95',
+    Оля: '50',
+    Саша: '20'
+}
 
-    const purpose = Number.parseFloat(prompt('Введите сумму которую желаете накопить?'));
-    print(`Целевая сумма: ${purpose} Руб`);
-    
-    const accumulatedIncome = getAccumulatedIncome(salary, extraMoney, expensesAmount);
-
-    const targetMonth = getTargetMonth(accumulatedIncome, purpose);
-    print(`Нужная сумма будет накоплена через: ${targetMonth} месяцев`);
-
-    const budgetDay = GetbudgetDay(accumulatedIncome)
-
-    console.clear()
-
-        if (budgetDay < 0) {
-        console.log('Что-то пошло не так'); 
-        } else if (budgetDay > 6000) {
-            console.log('У вас высокий уровень дохода');
-        } else if (budgetDay > 3000 && budgetDay < 6000) {
-        console.log('У вас средний уровень дохода');
-        } else if (budgetDay < 3000 && budgetDay > 0) {
-            console.log('К сожалению у вас уровень дохода ниже среднего');
-        }
-};
-
+for (const key in studentScores) {
+    if (studentScores[key] < 80) delete studentScores[key]
+}
+console.log(studentScores);
