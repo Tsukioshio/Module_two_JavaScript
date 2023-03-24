ea17d6d02b4136cf4fe8dc0b4336a585c8606688
unknown <tsukioshio1@gmail.com>
2023-03-24 19:52:17 +0800

    'homework-09'

--- homeworkDOM/index.js
+++ homeworkDOM/index.js
@@ -2,24 +2,37 @@
 
 let money = 0 // доходы
 let profit = 'фриланс'
-let extraMoney = 20000 // до доходы
+let extraMoney = 0 // доп доходы
 let expenses =  "питание, проезд"
 let amount = 0 // расходы
-let purpose = 0
-let period = 12
+let purpose = 2000000
+let period = 0
 let deposit = true
 
 const getAccumulatedIncome = () => {
   const sumMoneyElement = document.querySelector('#sumMoney');
-  sumMoneyElement.textContent = ((money + extraMoney) - amount).toString()
+  const result = (money + extraMoney) - amount;
+
+  sumMoneyElement.textContent = result.toString();
+  return result;
 }
+
+const getSumMoneyDay = () => {
+  const sumMoneyDayElement = document.querySelector('#sumMoneyDay');
+  const result = Math.floor((money + extraMoney) / 30);
+
+  sumMoneyDayElement.textContent = result.toString();
+  
+} 
 
 // let accumulatedIncome = getAccumulatedIncome()
 // let budgetDay = accumulatedIncome / 30
 
-// function getTargetMonth() {
-//   return purpose / accumulatedIncome
-// }
+function getTargetMonth(accumulatedIncome) {
+  const purposeElement = document.querySelector('#purpose');
+  const result = Math.ceil(purpose / accumulatedIncome)
+  purposeElement.textContent = (result).toString()
+}
 
 // console.log('Ваш бюджет на месяц с учетом ваших расходов составляет: ', getAccumulatedIncome());
 // console.log(`Ваша цель накопить ${purpose} с учетом всех ваших расходов будет достигнута через`, getTargetMonth() + ' месяца');
@@ -29,22 +42,27 @@ const getAccumulatedIncome = () => {
 function onInputMoney({target}) {
   const {value} = target
   money = +value
-
-  getAccumulatedIncome()
+  
 }
 
 function onInputExtraMoney({target}) {
   const {value} = target
   extraMoney = +value
-
-  getAccumulatedIncome()
+  
 }
 
 function onInputAmount({target}) {
   const {value} = target
   amount = +value
 
-  getAccumulatedIncome()
+}
+
+
+
+function calc() {
+  const accumulatedIncome =  getAccumulatedIncome();
+  getTargetMonth(accumulatedIncome)
+  getSumMoneyDay()
 }
 
 
