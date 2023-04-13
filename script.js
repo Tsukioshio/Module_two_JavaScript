fafd513f5c86d423a211188785ef70bf6d11c668
unknown <tsukioshio1@gmail.com>
2023-04-13 22:51:37 +0800

    'Изменен'

--- script.js
+++ script.js
@@ -1,112 +1,107 @@
-'use strict'
+const regForm = document.getElementById('reg_form');
+const authForm = document.getElementById('auth_form');
 
-function validateEmail() {
-    const {classList: wrapperClasses} = document.querySelector('.email-wrapper')
-    const {value: email, classList: inputClasses} = document.querySelector('#email')
+if (regForm) {
+    regForm.addEventListener('submit', event => {
+        event.preventDefault();
 
-if(!isEmpty(email)) {
-    wrapperClasses.remove('input_email')
+        if (validateForm(event.target)) {
+            const inputUserName = document.getElementById('username');
+            const inputPassword = document.getElementById('password');
+            localStorage.setItem('username', inputUserName.value);
+            localStorage.setItem('password', inputPassword.value);
+        }
+    });
 
-    const regExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ 
+}
 
-    if(!regExp.test(email)) {
-        inputClasses.add('error-box')
-        wrapperClasses.add('error-email-box')
-    } else {
-        inputClasses.remove('error-box')
-        wrapperClasses.remove('error-email-box')
+if (authForm) {
+    document.getElementById('auth_form').addEventListener('submit', event => {
+        const form = event.target;
+
+        event.preventDefault();
+
+        if (validateForm(form)) {
+            const inputUserName = document.getElementById('username');
+            const inputPassword = document.getElementById('password');
+
+            if (
+                localStorage.getItem('username') === inputUserName.value &&
+                localStorage.getItem('password') === inputPassword.value
+            ) {
+                location.replace("./result.html");
+            }
+            else {
+                const errBlock = form.querySelector('.error_text');
+                errBlock.innerText = 'Логин/Пароль не верные';
+                errBlock.style.display = 'block';
+            }
+        }
+    });
+}
+
+document.querySelectorAll('.autorization_entry-field input').forEach(input => {
+    input.addEventListener('focus', inputFocus);
+});
+
+document.querySelectorAll('.entry-checkbox input').forEach(input => {
+    input.addEventListener('change', inputFocus);
+});
+
+
+function validateForm(form) {
+    const arInputs = form.querySelectorAll('.entry-field_required input');
+    let isValid = true;
+    let formData = {};
+
+    for (let i = 0; i < arInputs.length; i++) {
+        let input = arInputs[i];
+
+        if (!inputValidation(input)) {
+            isValid = false;
+        }
+
+        formData[input.name] = input.value;
     }
-} else {
-    wrapperClasses.remove('error-email-box')
-    wrapperClasses.add('input_email')
-    inputClasses.add('error-box')
-}
+
+    return isValid;
 }
 
-// function validatePassword() {
-//     const errorBox = document.querySelector('#error-box')
-//     const {value: password, classList: inputClasses} = document.querySelector('#password')
 
-//     if(!isEmpty(password)) {
-//         errorBox.style.display = 'none'
+function inputValidation(input) {
+    const value = input.value;
+    const parentNode = input.parentNode;
+    let errMessage = '';
 
-//         if(password.length <= 8) {
-//             inputClasses.add('error-box')
-//             errorBox.textContent = 'Пароль должен содержать как минимум 8 символов'
-//             errorBox.style.display = 'block'
-//         } else {
-//             inputClasses.remove('error-box')
-//             errorBox.style.display = 'none'
-//         } 
-//     }
-//     else {
-//         errorBox.textContent = 'Поле обязательно для заполнения'
-//         errorBox.style.display = 'block'
-//         inputClasses.add('error-box')
-//         }
-//     }
-
-function validatePassword() {
-  const errorBox = document.querySelector('.password-wrapper')
-  const {value: password, classList: inputClasses} = document.querySelector('#password')
-
-  if(!isEmpty(password)) {
-      errorBox.style.display = 'none'
-
-      if(password.length <= 8) {
-          inputClasses.add('error-box')
-          errorBox.textContent = 'Пароль должен содержать как минимум 8 символов'
-          errorBox.style.display = 'block'
-      } else {
-          inputClasses.remove('error-box')
-          errorBox.style.display = 'none'
-      } 
-  }
-  else {
-      errorBox.textContent = 'Поле обязательно для заполнения'
-      errorBox.style.display = 'block'
-      inputClasses.add('error-box')
-      }
-  }
-
-
-
-
-function isEmpty(textValue) {
-    return textValue.length === 0
-}
-
-function signIn() {
-    const email = document.querySelector(`.input_email`).value
-    const password = document.querySelector(`.input_password`).value
-    const user = {email, password}
-    console.log(user)
-    document.location.href = './result.html'
-}
-
-function signUp() {
-    validateEmail()
-
-    const email = document.querySelector(`.input_email`).value
-    const password = document.querySelector(`.input_password`).value
-
-    localStorage.setItem('userEmail', email)
-
-    
-}
-
-function getUsername() {
-    const email = localStorage.getItem('userEmail')
-
-    const messageElement = document.querySelector(`#message`)
-
-    if(email) {
-        messageElement.textContent = `Вы вошли в систему, ${email}`
-    } else {
-        messageElement.textContent = 'Что-то пошло не так'
+    if (!value) {
+        errMessage = 'Поле обязательно для заполнения';
     }
+    else if (input.type === 'checkbox' && !input.checked) {
+        errMessage = 'Поле обязательно для заполнения';
+    }
+    else if (input.type === 'password' && value.length < 8) {
+        errMessage = 'Пароль должен содержать как минимум 8 символов';
+    }
+    else if (input.type === 'email' && !validateEmail(value)) {
+        errMessage = 'Некорректный email';
+    }
+
+    if (errMessage) {
+        parentNode.classList.add('entry-field_error');
+        parentNode.querySelector('.signature').innerText = errMessage;
+    }
+
+    return !errMessage;
 }
 
-function logout() {
-    localStorage.removeItem(`userEmail`)
+
+function inputFocus(event) {
+    event.target.parentNode.classList.remove('entry-field_error');
+}
+
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    return re.test(String(email).toLowerCase());
 }
